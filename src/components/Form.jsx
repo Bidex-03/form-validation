83743626930210db871d4de4bf4fe50fe67661dc
Bidex-03 <abdullahqaasim14@gmail.com>
2023-04-06 14:50:35 +0100

    feat: added input hook for validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,42 +1,106 @@
 import React from "react";
 
+import classes from "../CSS/error.module.css";
+import useInput from "../hooks/use-input";
 import Button from "../UI/button";
 import Card from "../UI/Card";
 
 const Form = () => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangedHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim !== "");
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangedHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.includes("@"));
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+    console.log(enteredEmail);
+
+    resetNameInput();
+    resetEmailInput();
+  }
+
   return (
-    <Card className="w-[80%] md:w-[50%] lg:w-[30%] m-auto mt-36 p-4">
-      <form className="flex flex-col">
+    <Card className="w-[80%] md:w-[50%] lg:w-[30%] m-auto mt-24 p-4">
+      <form onSubmit={formSubmissionHandler} className="flex flex-col">
         <h1 className="mb-4 text-center text-Mangolia text-sm md:text-xl lg:text-xl font-semibold">
           SIGN IN TO YOUR ACCOUNT
         </h1>
 
-        {/* <label htmlFor="name" className="text-Marine-blue font-bold">
-          Name
-        </label> */}
-        <input
-          className="text-Marine-blue font-semibold mt-2 mb-4 px-4 py-2 rounded-[5px] bg-Light-gray focus:outline-none"
-          type="text"
-          id="name"
-        />
+        <div className={"flex flex-col"}>
+          <label htmlFor="name" className="text-Marine-blue font-bold">
+            Name
+          </label>
+          <input
+            className="text-Marine-blue font-semibold mt-2 mb-4 px-4 py-2 rounded-[5px] bg-Light-gray focus:outline-none"
+            type="text"
+            id="name"
+            onChange={nameChangedHandler}
+            onBlur={nameBlurHandler}
+            value={enteredName}
+          />
+          {nameInputHasError && (
+            <p className="text-Strawberry-red">Name must not be empty</p>
+          )}
+          {/* <p className="text-Strawberry-red">Name must not be empty</p> */}
+        </div>
 
-        {/* <label htmlFor="email" className="text-Marine-blue font-bold">
-          Email
-        </label> */}
-        <input
-          className="text-Marine-blue font-semibold mt-2 mb-4 px-4 py-2 rounded-[5px] bg-Light-gray focus:outline-none"
-          type="email"
-          id="email"
-        />
+        <div className="flex flex-col">
+          <label htmlFor="email" className="text-Marine-blue font-bold">
+            Email
+          </label>
+          <input
+            className="text-Marine-blue font-semibold mt-2 mb-4 px-4 py-2 rounded-[5px] bg-Light-gray focus:outline-none"
+            type="email"
+            id="email"
+            onChange={emailChangedHandler}
+            onBlur={emailBlurHandler}
+            value={enteredEmail}
+          />
+          {nameInputHasError && (
+            <p className="text-Strawberry-red">Please provide a valid email address.</p>
+          )}
+          {/* <p className="text-Strawberry-red">Please provide a valid email address.</p> */}
+        </div>
 
         <div className="my-6">
           <input type="checkbox" className="mr-4" />
           <label className="text-gray">Keep me signed in</label>
         </div>
 
-        <Button>SIGN IN</Button>
+        <Button disabled={!formIsValid} onClick={formSubmissionHandler}>SIGN IN</Button>
 
-        <a href="#" className="text-center my-2">Forgot your password?</a>
+        {/* <div className="bg-Purplish-blue text-center text-xl text-White py-2 rounded-[5px] hover:bg-Marine-blue cursor-pointer">
+          <button disabled={!formIsValid}>SIGN IN</button>
+        </div> */}
+
+        <a href="#" className="flex text-center mx-auto my-2">
+          Forgot your password?
+        </a>
       </form>
     </Card>
   );
